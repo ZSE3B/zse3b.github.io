bc73fc3ff9aefede3e189d6fc86ccc8289d4ddd2
chaosbooy <lpmelon234@gmail.com>
2023-11-06 17:01:28 +0100

    małymi kroczkami do dużego celu (troche nie mamy na to czasu ale walić)
    babubiba

--- outfitadvisor/src/ChatGpt.js
+++ outfitadvisor/src/ChatGpt.js
@@ -1,12 +1,21 @@
 import {React} from 'react'
+import OpenAI from 'openai'
 
 const ChatGpt = () => {
 
-    
+    const API_KEY = "" // tutaj klucz na gpt
+
+    const openai = new OpenAI({
+        apiKey: API_KEY
+    })
+
+    const AskQuestion = (mess) => {
+
+    }
 
     return(
         <div id="ai">
-            pusto
+            już nie tak pusto
         </div>
     );
 }
